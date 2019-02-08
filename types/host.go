9b9cb65ea4172390869567262856b3fa64c396ad
David Zech <david.c.zech@intel.com>
2019-02-07 16:56:03 -0800

    Define types

--- types/host.go
+++ types/host.go
@@ -1,4 +1,9 @@
 package types
 
 type Host struct {
+	ID       string `json:"id"`
+	OS       string `json:"os"`
+	Hostname string `json:"hostname"`
+	Version  string `json:"version"`
+	Build    string `json:"build"`
 }
--- types/report.go
+++ types/report.go
@@ -1,4 +1,17 @@
 package types
 
 type Report struct {
+	ID        string `json:"id"`
+	HostID    string `json:"host_id"`
+	Detection struct {
+		Description      string `json:"detection"`
+		PID              int    `json:"pid"`
+		TID              int    `json:"tid"`
+		ProcessName      string `json:"process_name"`
+		ProcessImagePath string `json:"process_image_path"`
+		ProcessCmdLine   string `json:"process_cmd_line"`
+	} `json:"detection"`
+	Error struct {
+		Description string `json:"description"`
+	} `json:"error,omitempty"`
 }
