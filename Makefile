81f611c24050b4da92321b89813a7ee6d26f709e
David Zech <david.c.zech@intel.com>
2019-02-20 16:10:07 -0800

    Implement Linux Makeself installer

--- Makefile
+++ Makefile
@@ -6,12 +6,24 @@ VERSION := $(or ${GITTAG}, v0.0.0)
 .PHONY: tdservice installer docker all test clean
 
 tdservice:
-	env GOOS=linux go build -ldflags "-X intel/isecl/tdservice/version.Version=$(version) -X intel/isecl/tdservice/version.GitHash=$(GITCOMMIT") -o out/tdservice
+	env GOOS=linux go build -ldflags "-X intel/isecl/tdservice/version.Version=$(VERSION) -X intel/isecl/tdservice/version.GitHash=$(GITCOMMIT)" -o out/tdservice
 
 test:
 	go test ./... -coverprofile cover.out
 	go tool cover -func cover.out
 	go tool cover -html=cover.out -o cover.html
 
+
+installer: tdservice
+	mkdir -p out/installer
+	cp dist/linux/install.sh out/installer/install.sh && chmod +x out/installer/install.sh
+	cp out/tdservice out/installer/tdservice
+	makeself out/installer out/tdservice-$(VERSION)-$(GITCOMMIT).bin "Threat Detection Service $(VERSION)" ./install.sh
+
+all: test installer
+
+
 clean:
+	rm cover.*
+	rm tdservice
 	rm -rf out/
---
+++ dist/linux/install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# READ .env file 
+if [ -f ~/tdservice.env ]; then 
+    echo Reading Installation options from `realpath ~/tdservice.env`
+    source ~/tdservice.env
+elif [ -f ./wls.env ]; then
+    echo Reading Installation options from `realpath ./tdservice.env`
+    source ./tdservice.end
+fi
+
+# Export all known variables
+
+if [[ $EUID -ne 0 ]]; then 
+    echo "This installer must be run as root"
+    exit 1
+fi
+
+echo Setting up Threat Detection Service Linux User...
+id -u tds 2> /dev/null || useradd tds
+
+echo Installing Threat Detection Service...
+# cp tdservice /usr/local/bin/tdservice
+chmod +x /usr/local/bin/tdservice
+chmod +s /usr/local/bin/tdservice
+chown tds:tds /usr/local/bin/tdservice
+
+# Create configuration directory in /etc
+mkdir -p /etc/tdservice && chown tds:tds /etc/tdservice
+# Create run directory in /var/run
+mkdir -p /var/run/tdservice && chown tds:tds /var/run/tdservice
+# Create data dir in /var/lib
+mkdir -p /var/lib/tdservice && chown tds:tds /var/lib/tdservice
+# Create logging dir in /var/log
+mkdir -p /var/log/tdservice && chown tds:tds /var/log/tdservice
+
+# check if TDS_NOSETUP is defined
+if [[ -z $TDS_NOSETUP ]]; then 
+    echo Running setup tasks...
+    tdservice setup
+    SETUPRESULT=$?
+    echo Installation complete!
+    if [ ${SETUPRESULT} == 0 ]; then 
+        tdservice start
+    fi
+else 
+    echo flag TDS_NOSETUP is defined, skipping setup
+    echo Installation complete!
+fi
