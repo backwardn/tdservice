68c75740e1cdde4a64c49942152bd014879aede3
David Zech <david.c.zech@intel.com>
2019-02-21 14:05:09 -0800

    Fix broken tests

--- config/config_test.go
+++ config/config_test.go
@@ -12,17 +12,16 @@ func TestLoad(t *testing.T) {
 	temp, _ := ioutil.TempFile("", "config.yml")
 	temp.WriteString("port: 1337\ntds:\n")
 	defer os.Remove(temp.Name())
-	c, err := Load(temp.Name())
-	assert.NoError(t, err)
+	c := Load(temp.Name())
 	assert.Equal(t, 1337, c.Port)
 }
 
 func TestSave(t *testing.T) {
 	temp, _ := ioutil.TempFile("", "config.yml")
 	defer os.Remove(temp.Name())
-	c, _ := Load(temp.Name())
+	c := Load(temp.Name())
 	c.Port = 1337
 	c.Save()
-	c2, _ := Load(temp.Name())
+	c2 := Load(temp.Name())
 	assert.Equal(t, 1337, c2.Port)
 }
