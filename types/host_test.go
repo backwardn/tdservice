c30e8ea21355d4210e4fb1f1365fae6bf5c4fdc0
David Zech <david.c.zech@intel.com>
2019-02-22 20:54:44 -0800

    Update TestEncoding

--- types/host_test.go
+++ types/host_test.go
@@ -19,12 +19,12 @@ func TestEncoding(t *testing.T) {
 	}
 	j, _ := json.Marshal(h)
 	t.Log(string(j))
-	assert.Equal(t, `{"id":"1234","hostname":"","version":"v1.0","build":"12313131","os":"Linux","status":"offline"}`, string(j))
+	assert.Equal(t, `{"id":"1234","version":"v1.0","build":"12313131","os":"Linux","status":"offline"}`, string(j))
 }
 
 func TestDecoding(t *testing.T) {
 	var h Host
-	json.Unmarshal([]byte(`{"id":"1234","hostname":"","version":"v1.0","build":"12313131","os":"Linux","status":"offline"}`), &h)
+	json.Unmarshal([]byte(`{"id":"1234","version":"v1.0","build":"12313131","os":"Linux","status":"offline"}`), &h)
 	assert.Equal(t, "1234", h.ID)
 	assert.Equal(t, "v1.0", h.Version)
 }
