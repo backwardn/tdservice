ad17a52afcb36f7dc24638243334dba03e7541b8
R, Praveenraj <praveenraj.r@intel.com>
2019-03-28 02:14:45 -0700

    made changes in auth implementation to get user based on userid or username

--- middleware/auth.go
+++ middleware/auth.go
@@ -13,19 +13,21 @@ import (
 func NewBasicAuth(u repository.UserRepository) mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			username, password, ok := r.BasicAuth()
-			log.Debug("Attempting to authenticate user: ", username)
+			userNameorId, password, ok := r.BasicAuth()
+			log.Debug("Attempting to authenticate user: ", userNameorId)
 			if ok {
 				// fetch by user
-				user, err := u.Retrieve(types.User{Name: username})
+				user, err := u.Retrieve(types.User{Name: userNameorId})
 				if err != nil {
-					// log this
-					log.WithError(err).Error("BasicAuth failure: could not retrieve user")
-					w.WriteHeader(http.StatusUnauthorized)
-					return
+                	user, err = u.Retrieve(types.User{ID: userNameorId})
+					if err != nil {
+						log.WithError(err).Error("BasicAuth failure: could not retrieve user")
+						w.WriteHeader(http.StatusUnauthorized)
+						return
+					}
 				}
 				if err := user.CheckPassword([]byte(password)); err != nil {
-					log.WithError(err).Error("BasicAuth failure: password mismatch, user", username)
+					log.WithError(err).Error("BasicAuth failure: password mismatch, user", userNameorId)
 					w.WriteHeader(http.StatusUnauthorized)
 					return
 				}
